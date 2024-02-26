7e65ac79c857b0eaf46201b6d7e1f2efc9319174
MADElinessss <jyseen@naver.com>
2024-02-26 11:11:24 +0900

    [수업내용] TableView 리스트에 CRUD

--- SeSACMVVM/View/UserTableViewController.swift
+++ SeSACMVVM/View/UserTableViewController.swift
@@ -10,17 +10,67 @@ import UIKit
 class UserTableViewController: UITableViewController {
     
     let viewModel = UserViewModel()
-
+    
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var addTextField: UITextField!
+    
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var loadButton: UIButton!
+    @IBOutlet weak var deleteAllButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Food(name: "")
+        configureView()
+        
+        bindData()
+    }
+    
+    func configureView() {
+        addButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+        searchButton.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
+        loadButton.addTarget(self, action: #selector(loadButtonTapped), for: .touchUpInside)
+        deleteAllButton.addTarget(self, action: #selector(deleteAllButtonTapped), for: .touchUpInside)
+    }
+    
+    func bindData() {
+        // didSet해서 reloadData 했던거를 이렇게 바꿔
+        viewModel.list.bind { value in // value: [User]
+            self.tableView.reloadData()
+        }
+    }
+    
+    @objc func deleteAllButtonTapped(_ sender: UIButton) {
+        viewModel.inputDeleteButtonTapped.value = ()
+    }
+    
+    @objc func loadButtonTapped(_ sender: UIButton) {
+        // viewModel - fetchUser -> 다시 4명만 넣어주기
+        viewModel.inputLoadButtonTapped.value = () // 비어있는 튜플을 보낸다!
     }
 
+    
+    @objc func addButtonTapped(_ sender: UIButton) {
+        // viewModel > list > append
+        // viewModel.appendUser(name: addTextField.text)
+        
+        viewModel.inputAddText.value = addTextField.text
+        
+        // tableView.reloadData()
+    }
+    
+    @objc func searchButtonTapped() {
+        // 검색 기능
+        // VC에서 하던걸 -> 뷰모델
+        viewModel.inputSearchWord.value = searchTextField.text
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.numberOfRowsInSection
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
--- SeSACMVVM/ViewModel/UserViewModel.swift
+++ SeSACMVVM/ViewModel/UserViewModel.swift
@@ -8,19 +8,81 @@
 import Foundation
 
 class UserViewModel {
+
+    var list: Observable<[User]> = Observable([])
     
-    var list = [User(name: "Madeline", age: 27),
-                User(name: "Moana", age: 26),
-                User(name: "Musk", age: 24),
-                User(name: "A", age: 29),
-                User(name: "B", age: 30)]
+    // 검색 버튼 클릭 시 검색 키워드
+    var inputSearchWord: Observable<String?> = Observable("")
+    
+    // 추가 버튼 클릭시 추가할 텍스트 받아오는 변수
+    var inputAddText: Observable<String?> = Observable("")
+    
+    // 로드 버튼 탭했따!
+    var inputLoadButtonTapped: Observable<Void?> = Observable(nil)
+    
+    // 삭제 버튼 탭해따!
+    var inputDeleteButtonTapped: Observable<Void?> = Observable(nil)
+    
+    // 애초에 뷰모델에서 리스트에 데이터 넣기
+    init() {
+        fetchUser()
+        
+        inputLoadButtonTapped.bind { _ in
+            self.fetchUser()
+        }
+        
+        inputDeleteButtonTapped.bind { _ in
+            self.list.value.removeAll()
+        }
+        
+        inputAddText.bind { value in
+            self.appendUser(name: value)
+        }
+        
+        inputSearchWord.bind { value in
+            guard let value = value else { return }
+            let result = self.list.value.filter { $0.name.contains(value) }
+            self.list.value = result
+        }
+    }
     
     var numberOfRowsInSection: Int {
-        return list.count
+        return list.value.count
     }
     
     // indexPath.row 매개변수
     func cellForRowAt(_ indexPath: IndexPath) -> User {
-        return list[indexPath.row]
+        return list.value[indexPath.row]
+    }
+    
+    private func fetchUser() {
+        list.value = [User(name: "Madeline", age: 27),
+                      User(name: "Moana", age: 26),
+                      User(name: "Musk", age: 24),
+                      User(name: "A", age: 29),
+                      User(name: "B", age: 30)]
+    }
+    
+    func appendUser(name: String?) {
+        // 옵셔널 바인딩
+        guard let name = name else { return }
+        
+        let value = list.value.filter { $0.name == name }
+        if value.count == 0 {
+            let age = Int.random(in: 20...40)
+            let user = User(name: name, age: age)
+            list.value.append(user)
+        } else { return }
+        
+        // 중복 여부 확인 -> 비즈니스 로직
+//        for item in list {
+//            if item.name == name {
+//               return // 중복이니까 무시해
+//            } else {
+//                let age = Int.random(in: 20...40)
+//                let user = User(name: name, age: age)
+//                list.append(user)
+//            }
+//        }
     }
 }
